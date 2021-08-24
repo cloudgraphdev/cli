e755b41e813ef22245838ab3097d99df04ff0903
tyler dunkel <tyler.dunkel@autocloud.dev>
2021-08-24 12:23:06 -0500

    fix(init): update with log for where data will be stored

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -56,7 +56,7 @@ export default class Init extends Command {
     [key: string]: string | Record<string, unknown>
   }> {
     const {
-      flags: { dgraph, directory, 'query-engine': queryEngine },
+      flags: { dgraph, 'query-engine': queryEngine },
     } = this.parse(Init)
     const result: { [key: string]: any } = {}
     if (dgraph) {
@@ -73,19 +73,6 @@ export default class Init extends Command {
         },
       ])
       result.dgraphHost = dgraph
-    }
-    if (directory) {
-      result.directory = directory
-    } else {
-      const { inputDirectory } = await this.interface.prompt([
-        {
-          type: 'input',
-          message: 'What directory would you like CloudGraph to store data in?',
-          name: 'inputDirectory',
-          default: 'cg',
-        },
-      ])
-      result.directory = inputDirectory
     }
     if (queryEngine) {
       result.queryEngine = QueryEngine
@@ -117,7 +104,7 @@ export default class Init extends Command {
 
   async run() {
     const { argv, flags } = this.parse(Init)
-    const { configDir } = this.config
+    const { configDir, dataDir } = this.config
     // const opts: Opts = {logger: this.logger, debug, devMode}
     // First determine the provider if one has not been passed in args
     // if no provider is passed, they can select from a list of offically supported providers
@@ -212,6 +199,7 @@ export default class Init extends Command {
       JSON.stringify(configResult, null, 2)
     )
     this.logger.success(`Your config has been successfully stored at ${chalk.italic.green(path.join(configDir, '.cloud-graphrc.json'))}`)
+    this.logger.success(`Your data will be stored at ${chalk.italic.green(path.join(dataDir, this.versionDirectory))}`)
     this.exit()
   }
 }
