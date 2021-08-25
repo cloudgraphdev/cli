5621bddce062427be830e1401fb5d22699c36998
tyler dunkel <tyler.dunkel@autocloud.dev>
2021-08-25 10:08:46 -0500

    fix(voyager): add graphql-voyager dep

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,81 @@
-import {altairExpress} from 'altair-express-middleware'
 import express from 'express'
+import {altairExpress} from 'altair-express-middleware'
 import expressPlayground from'graphql-playground-middleware-express'
+// import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 
+function renderVoyagerPage(options: {endpointUrl: string}) {
+  const { endpointUrl } = options;
+  const version = '1.0.0-rc.31'
+  return `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=utf-8 />
+  <meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+  <title>GraphQL Voyager</title>
+  <style>
+    body {
+      padding: 0;
+      margin: 0;
+      width: 100%;
+      height: 100vh;
+      overflow: hidden;
+    }
+    #voyager {
+      height: 100vh;
+    }
+  </style>
+  <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/graphql-voyager@${version}/dist/voyager.css"
+  />
+  <script src="https://cdn.jsdelivr.net/fetch/2.0.1/fetch.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/react@16/umd/react.production.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/graphql-voyager@${version}/dist/voyager.min.js"></script>
+</head>
+<body>
+  <main id="voyager">
+    <h1 style="text-align: center; color: #5d7e86;"> Loading... </h1>
+  </main>
+  <script>
+    window.addEventListener('load', function(event) {
+      function introspectionProvider(introspectionQuery) {
+        return fetch('${endpointUrl}', {
+          method: 'post',
+          headers: Object.assign({}, {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          }),
+          body: JSON.stringify({query: introspectionQuery }),
+          credentials: 'omit',
+        }).then(function (response) {
+          return response.text();
+        }).then(function (responseBody) {
+          try {
+            return JSON.parse(responseBody);
+          } catch (error) {
+            return responseBody;
+          }
+        });
+      }
+      GraphQLVoyager.init(document.getElementById('voyager'), {
+        introspection: introspectionProvider,
+        displayOptions: ${JSON.stringify({})},
+      })
+    })
+  </script>
+</body>
+</html>
+`;
+}
+
+const voyagerMiddleware = function expressMiddleware(options: {endpointUrl: string}) {
+  return (_req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(renderVoyagerPage(options));
+    res.end();
+  };
+}
 export default class QueryEngine {
   constructor(port: string) {
     this.port = port
@@ -20,6 +94,8 @@ export default class QueryEngine {
           addQueryDepthLimit: 3,
         },
       }))
+
+      app.use('/voyager', voyagerMiddleware({ endpointUrl: `${host}/graphql` }));
 
       // TODO: rework QueryEngine to do this better and only serve one
       app.get('/playground', expressPlayground({ endpoint: `${host}/graphql` }))
