ef69d91cefa95c37e36039e90e74c7f54347e7be
Alejandro Cotroneo <alejandro.cotroneo@autocloud.dev>
2021-10-05 23:56:36 -0300

    feat(poc): more structure and json based engine

--- src/policyPacks/rules-provider.ts
+++ src/policyPacks/rules-provider.ts
@@ -1,78 +1,93 @@
-import { loadFilesSync } from "@graphql-tools/load-files";
-import path from "path";
-
-import lodash from 'lodash'
-
 import DgraphEngine from '../storage/dgraph';
-import rulesAws from "./rules-aws";
 
+import jsonpath, {PathComponent} from 'jsonpath'
+import JsonEvaluator from "./evaluators/json-evaluator";
+import JsEvaluator from "./evaluators/js-evaluator";
+import {ResourceData, RuleEvaluator} from "./evaluators/rule-evaluator";
+
+export interface Rule {
+  id: string;
+  description: string;
+  rationale: string;
+  gql: string;
+  resource: string;
+}
+
+export enum RuleResult {
+  PASS = 'PASS',
+  FAIL = 'FAIL',
+  MISSING = 'MISSING',
+}
+
+export interface RuleFinding {
+  id: string;
+  ruleId: string;
+  resourceId: string;
+  result: RuleResult,
+  // connections ...
+  // securityGroup: [{id: data.resource.id}]
+}
 
 export default class RulesProvider {
+  evaluators: RuleEvaluator<any>[] = [new JsonEvaluator(), new JsEvaluator()]
 
-  requirements = ['@cloudgraph/cg-provider-aws'] // we could use something like this
+  constructor(
+    private rules: Rule[],
+    private typenameToFieldMap: { [tn: string]: string },
+    private schemaTypeName: string) {
+  }
 
   getSchema = () => {
-    const f = loadFilesSync(path.join(__dirname), {
-      extensions: ['graphql'],
-    });
-    return f
+    const mainType = `
+    enum ${this.schemaTypeName}Result {
+      PASS
+      FAIL
+      MISSING
+    }
+    type ${this.schemaTypeName} @key(fields: "id") {
+      id: String! @id
+      ruleId: String!
+      resourceId: String!
+      result: ${this.schemaTypeName}Result @search
+      # connections
+       ${Object.keys(this.typenameToFieldMap).map((tn: string) =>
+      `${this.typenameToFieldMap[tn]}: [${tn}] @hasInverse(field: findings)`).join(` `)
+    }
+    }
+   `
+    const extensions = Object.keys(this.typenameToFieldMap).map((tn: string) =>
+      `extend type ${tn} {
+   findings: [${this.schemaTypeName}] @hasInverse(field: ${this.typenameToFieldMap[tn]})
+}`).join('\n')
+
+    return [mainType, extensions]
   }
 
-  getRules() {
+  getRuleEvaluators(): RuleEvaluator<any>[] {
     // @TODO - we need to define how this is going to be decided at runtime (configuration)
-    return rulesAws
+    return this.evaluators
   }
-
 
   getData = async (cli: DgraphEngine) => {
     const findings: any = [];
-    const rules = this.getRules()
-    for(const rule of rules) {
-      const { data } = await cli.query(rule.query) as any;
-
-      const dedupeIds = {} as any;
-      function dfs(currData: any, path: string[], level: number, res: any[]) {
-        // dfs - base case
-        if (path.length === level) {
-          const resource = lodash.get(currData, path)
-          if (dedupeIds[resource.id]) return
-          dedupeIds[resource.id] = 1
-
-          const result = rule.check(currData);
-          const finding = {
-            id: rule.id + '/' + resource.id + '/',
-            ruleId: rule.id,
-            resourceId: resource.id,
-            result,
-            // connections are easier this way for our case. we need to figure out how to set a dynamic name based on type
-            securityGroup: [{id: resource.id}]
-          }
-          res.push(finding)
-          return
-        }
-        // dfs - traverse
-        const pathUpToLevel = path.slice(0, level + 1)
-        const _subtree = lodash.get(currData, pathUpToLevel)
-        const subTree = Array.isArray(_subtree)? _subtree: [_subtree];
-        for(let i = 0; i < subTree.length; i++) {
-          // we replace the many connections by one (remove resource uncles)
-          lodash.set(currData, pathUpToLevel, subTree[i]);
-          dfs(currData, path, level+1, res);
-        }
-        //[dfs] we'd restore original graph here, but it's not needed
+    for (const rule of this.rules) {
+      try {
+        const {data} = await cli.query(rule.gql) as any;
+        console.log('executing rule with data', rule, data)
+        const result = await this.processRule(rule, data);
+        console.log('result: ', result)
+        findings.push(...result)
+      } catch (e) {
+        console.error(e)
       }
-      const res: any[] = findings; //
-      console.log('data: ', data)
-      dfs(data, rule.resource.split('.'), 0, res)
-      console.log(res);
     }
 
     return {
       connections: [],
       entities: [{
-        name: 'AwsFinding',
-        mutation: `mutation($input: [AddawsFindingInput!]!) {
-  addawsFinding(input: $input, upsert: true) {
+        name: this.schemaTypeName,
+        mutation: `mutation($input: [Add${this.schemaTypeName}Input!]!) {
+  add${this.schemaTypeName}(input: $input, upsert: true) {
     numUids
   }
 }`,
@@ -80,4 +95,88 @@ export default class RulesProvider {
       }]
     }
   }
+
+  private processRule = async (rule: Rule, data: any) => {
+    const res: any[] = []; //
+    const dedupeIds = {} as any;
+    const resourcePaths = jsonpath.nodes(data, rule.resource)
+    const evaluator = this.getRuleEvaluator(rule)
+
+    if (!evaluator) {
+      console.warn('cant process rule - unrecognized pattern', rule)
+      return []
+    }
+
+    // @NOTE: here we can evaluate things such as Data being empty (may imply rule to pass)
+    // or if we have no resources, or none pass, we can decide on that rule (+ some rule field)
+    for (let i = 0; i < resourcePaths.length; i++) {
+      const {path, value: resource} = resourcePaths[i];
+      if (!resource.id) {
+        // @NOTE: we'll support more complex rules in the future where you dont need a resource upfront
+        console.warn('Resource must have an id', resourcePaths[i]);
+        continue
+      }
+      if (dedupeIds[resource.id]) {
+        console.warn('Resource is duplicated, skipping', resource.id)
+        continue
+      }
+      dedupeIds[resource.id] = 1
+
+      if (path[0] !== '$') {
+        console.log('Is this case possible? how do we process it?', resourcePaths[i]);
+        continue;
+      }
+      const processedData = this.highlightPath(data, path);
+      const ruleResult = await this.processSingleResourceRule(rule, evaluator, {
+        data: processedData,
+        resource,
+        resourcePath: jsonpath.stringify(path)
+      })
+      if (ruleResult) {
+        res.push(ruleResult)
+      }
+    }
+    return res
+  }
+
+  private getRuleEvaluator = (rule: Rule) => {
+    for (let evaluator of this.getRuleEvaluators()) {
+      if (evaluator.canEvaluate(rule)) {
+        return evaluator
+      }
+    }
+  }
+  private processSingleResourceRule = async (rule: Rule, evaluator: RuleEvaluator<any>, data: ResourceData) => {
+    let result = await evaluator.evaluateSingleResource(rule, data);
+
+    const finding = {
+      id: rule.id + '/' + data.resource.id,
+      ruleId: rule.id,
+      resourceId: data.resource.id,
+      result,
+    }
+    const connField = data.resource.__typename && data.resource.__typename[this.schemaTypeName]
+    if (connField) {
+      (finding as any)[connField] = [{id: data.resource.id}]
+    }
+    return finding
+  }
+
+  /**
+   * traverse the path, and 'highlight' the path towards the resource
+   * a[0].b.c[3].id === a['@'].b.c['@'].id // so rules can use this notation to know their parents
+   */
+  private highlightPath(data: any, path: PathComponent[]) {
+    let curr = data // we can write the data, as next time we'll set the same fields
+    for (let j = 1; j < path.length; j++) {
+      const segment = path[j]
+      if (Array.isArray(curr)) {
+        // this is an array, we store in []._ the alias of this resource position in the array
+        (curr as any)['@'] = curr[segment as number]
+      }
+      curr = curr[segment]
+    }
+    return data;
+  }
+
 }
