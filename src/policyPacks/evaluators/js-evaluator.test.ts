1bb2eaecb02c690629aac906a85195917faf7cdd
Alejandro Cotroneo <alejandro.cotroneo@autocloud.dev>
2021-10-13 14:28:05 -0300

    feat(poc): adjust rules

--- src/policyPacks/evaluators/js-evaluator.test.ts
+++ src/policyPacks/evaluators/js-evaluator.test.ts
@@ -5,26 +5,20 @@ describe('JsEvaluator', () => {
   it('should accept all rules that have a check field', () => {
     const e = new JsEvaluator()
 
-    expect(e.canEvaluate({} as any)).toBe(false)
-    expect(e.canEvaluate({ checks: 1 } as any)).toBe(false)
+    expect(e.canEvaluate({} as never)).toBe(false)
+    expect(e.canEvaluate({ checks: 1 } as never)).toBe(false)
 
     // we could improve these, but following tests will pass
-    expect(e.canEvaluate({ check: 1 } as any)).toBe(true)
-    expect(e.canEvaluate({ check: 0 } as any)).toBe(true)
-    expect(
-      e.canEvaluate({
-        check: () => {
-          // Empty
-        },
-      } as any)
-    ).toBe(true)
+    expect(e.canEvaluate({ check: 1 } as never)).toBe(true)
+    expect(e.canEvaluate({ check: 0 } as never)).toBe(true)
+    expect(e.canEvaluate({ check: () => 1 } as never)).toBe(true)
   })
 
   it('should call check with data', () => {
     const e = new JsEvaluator()
     const spy = jest.fn()
     const data = 'asdf'
-    e.evaluateSingleResource({ check: spy } as any, data as any)
+    e.evaluateSingleResource({ check: spy } as never, data as never)
 
     expect(spy).toHaveBeenCalledWith(data)
   })
@@ -34,11 +28,11 @@ describe('JsEvaluator', () => {
     const spy = jest.fn()
     spy.mockReturnValue(false)
     expect(
-      await e.evaluateSingleResource({ check: spy } as any, 0 as any)
+      await e.evaluateSingleResource({ check: spy } as never, 0 as never)
     ).toEqual(RuleResult.DOESNT_MATCH)
     spy.mockReturnValue(true)
     expect(
-      await e.evaluateSingleResource({ check: spy } as any, 0 as any)
+      await e.evaluateSingleResource({ check: spy } as never, 0 as never)
     ).toEqual(RuleResult.MATCHES)
   })
 })
