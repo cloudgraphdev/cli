7179902d8e10e04ab3b6479ff0ba4d1da337192c
Marco Franceschi <marco.franceschi@autocloud.dev>
2021-10-13 14:52:23 -0300

    fix: Refactor manager plugin

--- src/commands/rules.ts
+++ src/commands/rules.ts
@@ -4,17 +4,10 @@ import RulesProvider, {
   ruleSchemaTypeName,
 } from '@cloudgraph/policy-pack-aws'
 
-import { mergeTypeDefs } from '@graphql-tools/merge'
-import { print } from 'graphql'
-
 import Command from './base'
-import { processConnectionsBetweenEntities } from '../utils'
+// import { processConnectionsBetweenEntities } from '../utils'
 import DgraphEngine from '../storage/dgraph'
-
-function mergeSchemas(currSchema: string, additions: string[]) {
-  const s = mergeTypeDefs([currSchema, ...additions])
-  return print(s)
-}
+import { mergeSchemas } from '../utils/schema'
 
 export default class Rules extends Command {
   static description = 'Local test'
@@ -37,7 +30,7 @@ export default class Rules extends Command {
     // } = this.parse(Rules)
     // Run dgraph health check
     const storageEngine = this.getStorageEngine()
-    const storageRunning = await storageEngine.healthCheck()
+    // const storageRunning = await storageEngine.healthCheck()
 
     const client = new RulesProvider(
       awsRules,
@@ -58,11 +51,10 @@ export default class Rules extends Command {
      *  Then the rules 'plugin' returns its 'append only' schema updates.
      *  Note that we can even create bidirectional connections with append-only
      */
-    const currSchema: string = await dGraph.getSchema()
-    console.log('The scmea', currSchema)
+    const currentSchema: string = await dGraph.getSchema()
     const providerSchema: string[] = client.getSchema()
-    // console.log('new schema', providerSchema)
-    await storageEngine.setSchema([mergeSchemas(currSchema, providerSchema)])
+
+    await storageEngine.setSchema([mergeSchemas(currentSchema, providerSchema)])
 
     /**
      * Run rules:
@@ -70,16 +62,17 @@ export default class Rules extends Command {
      * we'd not be covering things such as 'deletes'. It's useful for local development, but lets try to run them once per version
      * same interface as Providers, but we pass dGraph client - previous data needs to be saved already
      */
-    const providerData = await client.getData(dGraph)
+    // const providerData = await client.getData(dGraph)
+    await client.getData(dGraph)
 
     // @NOTE: processConnectionsBetweenEntities is not needed as we can return the right mutations from within the rules plugin
     // we only use it as it has the report, and to push the mutations
-    processConnectionsBetweenEntities(
-      providerData,
-      storageEngine,
-      storageRunning
-    )
-    await storageEngine.run(false)
+    // processConnectionsBetweenEntities(
+    //   providerData,
+    //   storageEngine,
+    //   storageRunning
+    // )
+    // await storageEngine.run(false)
     this.exit()
   }
 }
