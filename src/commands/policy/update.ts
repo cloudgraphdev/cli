c067e12fbadbdfcec24255f1709a7b7c1df1bf59
Marco Franceschi <marco.franceschi@autocloud.dev>
2021-11-30 12:49:24 -0300

    chore: Updated policy update naming

--- src/commands/policy/update.ts
+++ src/commands/policy/update.ts
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import Command from '../base'
 import { PluginType } from '../../utils/constants'
 
-const getProvider = (val: string): string =>
+const getPolicy = (val: string): string =>
   val.includes('@') ? val.split('@')[0] : val
 
 export default class Update extends Command {
@@ -12,9 +12,9 @@ export default class Update extends Command {
   static aliases = ['update']
 
   static examples = [
-    '$ cg provider update',
-    '$ cg provider update aws',
-    '$cg provider update aws@0.12.0',
+    '$ cg policy update',
+    '$ cg policy update aws',
+    '$cg policy update aws@0.12.0',
   ]
 
   static strict = false
@@ -33,37 +33,37 @@ export default class Update extends Command {
     const manager = this.getPluginManager(PluginType.PolicyPack)
     const lockFile = manager.getLockFile()
 
-    // Get the providers from the lock file that user wants to update
+    // Get the policiess from the lock file that user wants to update
     // If user passes something like aws@1.1.0, filter the lock file to only grab 'aws' entry
     const policyPacksList =
       allPolicyPacks.length >= 1
         ? pickBy(lockFile, (_, key) => {
-            const providers = allPolicyPacks.map(val => {
-              return getProvider(val)
+            const policiess = allPolicyPacks.map(val => {
+              return getPolicy(val)
             })
-            return providers.indexOf(key) > -1
+            return policiess.indexOf(key) > -1
           })
         : lockFile.policyPack
 
-    // Warn the user if they are trying to update providers they have not installed.
-    const nonInstalledProviders = allPolicyPacks.filter(rawProvider => {
-      const provider = getProvider(rawProvider)
-      return Object.keys(lockFile).includes(provider)
+    // Warn the user if they are trying to update policies they have not installed.
+    const nonInstalledPoliciess = allPolicyPacks.filter(rawPolicy => {
+      const policy = getPolicy(rawPolicy)
+      return Object.keys(lockFile).includes(policy)
     })
-    for (const provider of nonInstalledProviders) {
+    for (const policyPackName of nonInstalledPoliciess) {
       this.logger.warn(
         `${chalk.green(
-          getProvider(provider)
+          getPolicy(policyPackName)
         )} not found in lock file, have you installed it?`
       )
     }
 
-    // Loop through providers and try to update them
+    // Loop through policies and try to update them
     for (const [key] of Object.entries(policyPacksList)) {
       let version = 'latest'
-      const rawProvider = allPolicyPacks.find(val => val.includes(key))
-      if (rawProvider && rawProvider.includes('@')) {
-        [, version] = rawProvider.split('@')
+      const rawP = allPolicyPacks.find(val => val.includes(key))
+      if (rawP && rawP.includes('@')) {
+        [, version] = rawP.split('@')
       }
       await manager.getPlugin(key, version)
       this.logger.info(
