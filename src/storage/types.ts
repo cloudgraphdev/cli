a32bcf5b8d70a0a31ccf984a62890ee5bccaa065
Marco Franceschi <marco.franceschi@autocloud.dev>
2021-12-17 11:51:18 -0300

    feat: Replaced Dgraph types from SDK

--- src/storage/types.ts
+++ src/storage/types.ts
@@ -1,6 +1,4 @@
-import { Logger } from '@cloudgraph/sdk'
 import { Method } from 'axios'
-import { ExecutionResult } from 'graphql'
 
 export interface RequestConfig {
   baseUrl?: string
@@ -13,32 +11,4 @@ export interface RequestConfig {
 export interface GraphQLFormattedQuery {
   query: string
   variables: any
-}
-
-export interface GraphQLInputData {
-  query: string
-  connectedData: any
-  name: string
-}
-
-export interface StorageEngineConnectionConfig {
-  scheme: string
-  host: string
-  port: string
-}
-
-export interface StorageEngineConfig extends StorageEngineConnectionConfig {
-  type: string
-  logger: Logger
-}
-
-export interface StorageEngine {
-  host: string
-  logger: Logger
-  healthCheck: (showInitialStatus?: boolean) => Promise<boolean>
-  setSchema: (schema: string[]) => Promise<void>
-  dropAll: () => Promise<ExecutionResult>
-  dropData: () => Promise<ExecutionResult>
-  push: (data: any) => void
-  run: (dropData?: boolean) => Promise<void>
 }
